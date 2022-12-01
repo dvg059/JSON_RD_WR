5a88389e57bc6c746f57fe2f4f45a9ce3c71bbd7
dvg059 <dom.vag@gmail.com>
2022-12-01 15:14:32 -0500

    first commit

---
+++ cpp/json_rd_wr/src/json_rd_wr.h
@@ -0,0 +1,6 @@
+#ifndef JSON_RD_WR
+#define JSON_RD_WR
+#include <string>
+
+
+#endif // JSON_RD_WR
---
+++ cpp/json_rd_wr/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include "json_rd_wr.h"
+
+int main(int argc, char **argv){
+    std::cout << "Hello JASON_RD_WR" << std::endl;
+}
