fc5d253d46a62bdf1d8139b1ae92d50ccb3f92ea
dvg059 <dom.vag@gmail.com>
2022-12-02 14:06:00 -0500

    Delete cpp directory

--- cpp/json_rd_wr/src/json_rd_wr.h
+++
@@ -1,6 +0,0 @@
-#ifndef JSON_RD_WR
-#define JSON_RD_WR
-#include <string>
-
-
-#endif // JSON_RD_WR
--- cpp/json_rd_wr/src/main.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "json_rd_wr.h"
-
-int main(int argc, char **argv){
-    std::cout << "Hello JASON_RD_WR" << std::endl;
-}
